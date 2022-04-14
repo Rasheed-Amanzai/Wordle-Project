1cf56e13491ea4efe4992a3a0991bce452613bb7
Fio <fiorellarozas@hotmail.com>
2022-04-13 23:15:03 -0400

    Implemented auto-properties for class Player

--- Wordle-Project/Wordle-Project/Player.cs
+++ Wordle-Project/Wordle-Project/Player.cs
@@ -4,8 +4,25 @@ using System.Text;
 
 namespace Wordle_Project
 {
+
     public class Player
     {
+        //how many words did player guessed correctly
+        public int Score { get; private set; } 
+        //player's highest score
+        public int HighScore { get; private set; }
+
+        //how many guesses does it take player to guess the word correctly
+        public int GuessAccuracy { get; private set; }
+
+        //percent of winning for player 
+        public double ChancesOfWinning { get; private set; }
+
+        //numOfGamesWon(_score)/numOfGamesPlayed
+        public int WinningStreak { get; private set; }
+
+
+
 
     }
 }
