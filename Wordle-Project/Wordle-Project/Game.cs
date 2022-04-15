c02e7328bb0a86bb444a1e974e7079db5dedb623
Rasheed <amanzair@sheridancollege.ca>
2022-04-14 22:47:28 -0400

    Implemented attributes/methods for class Game and Word. Also created text files containing the list of words (one full list of words, the other list containing the viable target words to be used).

--- Wordle-Project/Wordle-Project/Game.cs
+++ Wordle-Project/Wordle-Project/Game.cs
@@ -6,6 +6,18 @@ namespace Wordle_Project
 {
     public class Game
     {
+        private int _maxNumTurns = 6;
 
+        public int CurrentTurn { get; private set; }
+
+        public Player WordlePlayer = new Player();
+
+        public void StartGame()
+        {
+            CurrentTurn = 0;
+            
+            Word word = new Word();
+            word.GenerateTargetWord();
+        }
     }
 }
--- Wordle-Project/Wordle-Project/Word.cs
+++ Wordle-Project/Wordle-Project/Word.cs
@@ -1,11 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace Wordle_Project
 {
     public class Word
     {
+        // List of common words that the program will choose from as the word that needs to be guessed
+        private List<string> _usuableWords = File.ReadAllLines(@"UsuableWords.txt").ToList();
 
+        // Full list of words, any of these words will be viable as a guess
+        private List<string> _fullWordList = File.ReadAllLines(@"FullWordList.txt").ToList();
+
+        // The word that the player has to guess
+        public string TargetWord { get; private set; }
+
+        public void GenerateTargetWord()
+        {
+            var random = new Random();
+            int index = random.Next(_usuableWords.Count); // Gets a random index
+            TargetWord = _usuableWords[index];
+        }
+
+        public int CheckPlayersGuess(string guess)
+        {
+            if (guess == TargetWord)
+            {
+                // Guess is correct
+                return 1;
+            }
+            else if (_fullWordList.Contains(guess))
+            {
+                // Guess is incorrect, but is a valid word
+                return 2;
+            }
+            else
+            {
+                // Guess is a invalid word
+                return 3;
+            }
+        }
     }
 }
