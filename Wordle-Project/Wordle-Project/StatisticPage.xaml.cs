8a24987013cfc0867f6ebefcddc779f0fbda17f0
Fio <fiorellarozas@hotmail.com>
2022-04-19 01:26:34 -0400

    Added the title of the game and a button that guides user to the InstructionPage in WelcomePage UI

---
+++ Wordle-Project/Wordle-Project/StatisticPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace WordleProject
+{
+    public partial class StatisticPage : ContentPage
+    {
+        public StatisticPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Wordle-Project/Wordle-Project/WelcomePage.xaml.cs
+++ Wordle-Project/Wordle-Project/WelcomePage.xaml.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
-namespace Wordle_Project
+namespace WordleProject
 {
     public partial class WelcomePage : ContentPage
     {
         public WelcomePage()
         {
             InitializeComponent();
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new InstructionPage());
         }
     }
 }
