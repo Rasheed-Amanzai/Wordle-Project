c22879a7b68596dbff51f8ecc455dd37ee0d620f
Fio <fiorellarozas@hotmail.com>
2022-04-21 14:20:01 -0400

    Fixed Player's field variable names and filled statistic page labels

--- Wordle-Project/Wordle-Project/Player.cs
+++ Wordle-Project/Wordle-Project/Player.cs
@@ -8,9 +8,9 @@ namespace Wordle_Project
     public class Player
     {
         //how many words did player guessed correctly
-        public int Score { get; private set; } 
+        public int Score { get; set; } 
         //player's highest score
-        public int HighScore { get; private set; }
+        public int HighestScore { get; private set; }
 
         //how many guesses does it take player to guess the word correctly
         public int GuessAccuracy { get; private set; }
@@ -21,7 +21,10 @@ namespace Wordle_Project
         //numOfGamesWon(_score)/numOfGamesPlayed
         public int WinningStreak { get; private set; }
 
-
+        public Player()
+        {
+            Score = 11;
+        }
 
 
     }
--- Wordle-Project/Wordle-Project/StatisticPage.xaml.cs
+++ Wordle-Project/Wordle-Project/StatisticPage.xaml.cs
@@ -1,15 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 using Xamarin.Forms;
 
-namespace WordleProject
+namespace Wordle_Project
 {
     public partial class StatisticPage : ContentPage
     {
         public StatisticPage()
         {
+
             InitializeComponent();
+            Player plyr = new Player();
+
+            string ScoreLabelInt = Convert.ToString(plyr.Score);
+            ScoreLabel.Text = ScoreLabelInt;
+
+            string HighestScoreLabelInt = Convert.ToString(plyr.HighestScore);
+            HighestScoreLabel.Text = HighestScoreLabelInt;
+
+            string GuessAccuracyInt = Convert.ToString(plyr.GuessAccuracy);
+            GuessAccuracyLabel.Text = GuessAccuracyInt;
+
+            string ChancesOfWinningInt = Convert.ToString(plyr.ChancesOfWinning);
+            HighestScoreLabel.Text = HighestScoreLabelInt;
+
+
+
+
+
         }
+
+        void BackToGame_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+
+
     }
 }
