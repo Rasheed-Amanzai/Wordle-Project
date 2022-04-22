7396f8f4f0cfc243683aeaae64880b47fdd68f6b
Fio <fiorellarozas@hotmail.com>
2022-04-21 20:01:20 -0400

    * WelcomePage.xaml: Made 2 other buttons in WelcomePage.xaml that   redirects to instruction page and statistic page
    * WelcomePage.xaml.cs: Made 2 other buttons in WelcomePage that
      redirects to instruction page and statistic page

--- Wordle-Project/Wordle-Project/WelcomePage.xaml.cs
+++ Wordle-Project/Wordle-Project/WelcomePage.xaml.cs
@@ -12,9 +12,21 @@ namespace Wordle_Project
             InitializeComponent();
         }
 
-        void Button_Clicked(System.Object sender, System.EventArgs e)
+     
+
+        void StartGameButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+
+        void InstructionsButton_Clicked(System.Object sender, System.EventArgs e)
         {
             Navigation.PushAsync(new InstructionPage());
+        }
+
+        void StatsButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new StatisticPage());
         }
     }
 }
